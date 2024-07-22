bad441a8bf8de1f2fe782bde39136b23a4392ed9
Prinu Vinod <prinuvinod@gmail.com>
2024-07-22 22:34:23 +0530

    updated the website

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/location')
+def location():
+    return render_template('location.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
